067dc79e205ad7e669ad4fb2190e6d67c9e7bda3
dhruvinity <support@fifonetwork.com>
2018-04-12 05:11:26 +0530

    push with new build

--- dist/spa-mat/statics/matches.js
+++ dist/spa-mat/statics/matches.js
@@ -1,27 +1,27 @@
 var matches =[
     {
         opponent_one:'India',
-        opponent_one_logo:'../assets/indiaflag.png',
+        opponent_one_logo:'/assets/indiaflag.png',
         opponent_two:'Australia',
-        opponent_two_logo:'../assets/australianflag.jpeg'
+        opponent_two_logo:'/assets/australianflag.jpeg'
     },
     {
         opponent_one:'England',
-        opponent_one_logo:'../assets/englandflag.png',
+        opponent_one_logo:'/assets/englandflag.png',
         opponent_two:'Pakistan',
-        opponent_two_logo:'../assets/pakistanflag.jpeg'
+        opponent_two_logo:'/assets/pakistanflag.jpeg'
     },
     {
         opponent_one:'South Africa',
-        opponent_one_logo:'../assets/Saflag.png',
+        opponent_one_logo:'/assets/Saflag.png',
         opponent_two:'West Indies',
-        opponent_two_logo:'../assets/westindies.png'
+        opponent_two_logo:'/assets/westindies.png'
     },
     {
         opponent_one:'Zimbawe',
-        opponent_one_logo:'../assets/zimbabweflag.png',
+        opponent_one_logo:'/assets/zimbabweflag.png',
         opponent_two:'Bangladesh',
-        opponent_two_logo:'../assets/englandflag.png'
+        opponent_two_logo:'/assets/englandflag.png'
     }
 ]
 
